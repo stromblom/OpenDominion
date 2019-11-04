1ecf24d3d1dc4fa870cdfced699e942660c21af2
John Strömblom <john.stromblom@outlook.com>
2019-11-04 20:07:05 +0100

    Missed some palces

--- app/resources/views/layouts/topnav.blade.php
+++ app/resources/views/layouts/topnav.blade.php
@@ -46,7 +46,7 @@
 
                 <!-- Navbar Header -->
                 <div class="navbar-header">
-                    <a href="{{ url('') }}" class="navbar-brand">Open<b>Dominion</b></a>
+                    <a href="{{ url('') }}" class="navbar-brand"><span class="text-red">Temp</span><b>Dominion</b></a>
                     <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
                         <i class="fa fa-bars"></i>
                     </button>
