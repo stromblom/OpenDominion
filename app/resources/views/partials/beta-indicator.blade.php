9db9e6dabbc48289a4a8a0e4598557b1cda9fe2f
John Strömblom <john.stromblom@outlook.com>
2019-11-04 15:11:15 +0100

    Added special debug bar

--- app/resources/views/partials/beta-indicator.blade.php
+++ app/resources/views/partials/beta-indicator.blade.php
@@ -6,4 +6,8 @@
     <div class="bg-red" style="padding: 10px 15px; z-index: 999999; font-size: 12px; font-weight: 400; color: #fff;">
         <i class="icon fa fa-warning"></i> This instance of OpenDominion is running on a <b>local</b> environment and should not be used for production purposes.
     </div>
+@elseif (App::environment() === 'production')
+    <div class="bg-red" style="padding: 10px 15px; z-index: 999999; font-size: 12px; font-weight: 400; color: #fff;">
+        <i class="icon fa fa-warning"></i> This instance of OpenDominion is running on a <b>temporary</b> environment, until the Emperor returns.
+    </div>
 @endif
